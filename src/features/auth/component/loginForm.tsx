46ae3293072559de19a8919a09a7547c676c9e31
rita-scaletech <rita.savaliya@scaletech.xyz>
2023-03-27 11:33:25 +0530

    remove static credential for login

--- src/features/auth/component/loginForm.tsx
+++ src/features/auth/component/loginForm.tsx
@@ -1,29 +1,29 @@
-import React, { useCallback, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import React, { useCallback, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
-import * as Yup from 'yup';
-import { Formik, FormikValues, Field, ErrorMessage, Form } from 'formik';
-import { v4 as uuidv4 } from 'uuid';
+import * as Yup from "yup";
+import { Formik, FormikValues, Field, ErrorMessage, Form } from "formik";
+import { v4 as uuidv4 } from "uuid";
 
-import * as actionTypes from 'store/actionTypes';
-import AuthService from 'shared/services/auth.service';
-import HttpService from 'shared/services/http.service';
-import { API_CONFIG } from 'shared/constants/api';
-import { createAction } from 'shared/util/utility';
+import * as actionTypes from "store/actionTypes";
+import AuthService from "shared/services/auth.service";
+import HttpService from "shared/services/http.service";
+import { API_CONFIG } from "shared/constants/api";
+import { createAction } from "shared/util/utility";
 import {
     HidePasswordIcon,
     ShowPasswordIcon,
-} from 'shared/components/icons/icons';
-import { PASSWORD_REGEX } from 'shared/constants';
-import { notify } from 'shared/components/notification/notification';
+} from "shared/components/icons/icons";
+import { PASSWORD_REGEX } from "shared/constants";
+import { notify } from "shared/components/notification/notification";
 
 const LoginForm: React.FC = () => {
     const [showPassword, setShowPassword] = useState(false);
     const [loading, setLoading] = useState(false);
     const [signUp, setSignUp] = useState(false);
-    const [uuid, setUuid] = useState('');
-    const [userId, setUserId] = useState('');
+    const [uuid, setUuid] = useState("");
+    const [userId, setUserId] = useState("");
     const [totalAmount, setTotalAmount] = useState(0);
 
     const dispatch = useDispatch();
@@ -31,39 +31,38 @@ const LoginForm: React.FC = () => {
 
     const onSubmit = useCallback(
         (values: FormikValues) => {
+            console.log(values);
+
             const params = {
-                username: 'hetvi',
-                password: '1234',
+                username: values.username,
+                password: values.password,
             };
 
             setLoading(true);
             HttpService.post(API_CONFIG.path.login, params)
                 .then((res) => {
-                    const { data, userId, totalAmount } = res;
-                    console.log('.then ~ res:', res);
-                    data && AuthService.setAuthData(data);
-                    dispatch(createAction(actionTypes.AUTH_SUCCESS));
-                    dispatch(createAction(actionTypes.UPDATE_USER_DATA, data));
-                    setUserId(userId);
-                    setTotalAmount(totalAmount);
                     setLoading(false);
-                    navigate('/dashboard');
-                    notify('User successfully logged in.', 'success');
+                    res.data && AuthService.setAuthData(res.data.token);
+                    dispatch(createAction(actionTypes.AUTH_SUCCESS));
+                    dispatch(
+                        createAction(actionTypes.UPDATE_USER_DATA, res.data)
+                    );
+                    navigate("/");
                 })
                 .catch((err: Error) => {
                     setLoading(false);
                     dispatch(createAction(actionTypes.AUTH_FAILED));
-                    console.error('Error', err);
+                    console.log("Error", err);
                 });
         },
         [dispatch]
     );
 
     const handleGuestUser = () => {
-        const uuid = localStorage.getItem('uuid');
+        const uuid = localStorage.getItem("uuid");
         if (!uuid) {
             const generateUuid = uuidv4();
-            localStorage.setItem('uuid', generateUuid);
+            localStorage.setItem("uuid", generateUuid);
             setUuid(generateUuid);
         } else {
             setUuid(uuid);
@@ -93,49 +92,45 @@ const LoginForm: React.FC = () => {
 						className='text--red-400 font-size--xxs pl--10 error-message mt--5'
 					/>
 				</div>} */}
-                <div className='form-item mb--25 position--relative'>
+                <div className="form-item mb--25 position--relative">
                     <Field
-                        readOnly
-                        name='username'
-                        type='username'
-                        className='input-field'
-                        autoComplete='off'
-                        placeholder='Enter Your Name'
-                        value='hetvi'
+                        name="username"
+                        type="username"
+                        className="input-field"
+                        autoComplete="off"
+                        placeholder="Enter Your Name"
                     />
                     <ErrorMessage
-                        name='username'
-                        component='p'
-                        className='text--red-400 font-size--xxs pl--10 error-message mt--5'
+                        name="username"
+                        component="p"
+                        className="text--red-400 font-size--xxs pl--10 error-message mt--5"
                     />
                 </div>
-                <div className='form-item mb--45 position--relative'>
+                <div className="form-item mb--45 position--relative">
                     <Field
-                        readOnly
-                        name='password'
-                        type={showPassword ? 'text' : 'password'}
-                        className='input-field'
-                        autoComplete='off'
-                        placeholder='Password'
-                        value='1234'
+                        name="password"
+                        type={showPassword ? "text" : "password"}
+                        className="input-field"
+                        autoComplete="off"
+                        placeholder="Password"
                     />
                     <div
-                        className='password-icon position--absolute  flex cursor--pointer align-items--center'
+                        className="password-icon position--absolute  flex cursor--pointer align-items--center"
                         onClick={() => setShowPassword(!showPassword)}>
                         {showPassword ? (
-                            <ShowPasswordIcon className='fill--comet' />
+                            <ShowPasswordIcon className="fill--comet" />
                         ) : (
                             <HidePasswordIcon />
                         )}
                     </div>
                     <ErrorMessage
-                        name='password'
-                        component='p'
-                        className='text--red-400 font-size--xxs pl--10 error-message mt--5'
+                        name="password"
+                        component="p"
+                        className="text--red-400 font-size--xxs pl--10 error-message mt--5"
                     />
                 </div>
-                <button disabled={loading} className='login-btn' type='submit'>
-                    {signUp ? 'Sign Up' : 'Login'}
+                <button disabled={loading} className="login-btn" type="submit">
+                    {signUp ? "Sign Up" : "Login"}
                 </button>
                 {/* <div className='flex align-items--center justify-content--end mt--10'>
 					<Link
@@ -169,18 +164,18 @@ const LoginForm: React.FC = () => {
 };
 
 const initialValues = {
-    email: '',
-    password: '',
+    email: "",
+    password: "",
 };
 
 const loginFormValidationSchema = Yup.object().shape({
     // email: Yup.string().email('Please Enter Valid Email').required('Please Enter Email').strict(true),
-    username: Yup.string().required('UserName is Required').strict(true),
+    username: Yup.string().required("UserName is Required").strict(true),
     password: Yup.string()
-        .required('Please Enter Password')
+        .required("Please Enter Password")
         .matches(
             PASSWORD_REGEX,
-            'Must Contain 8 Characters, One Number and One Special Case Character '
+            "Must Contain 8 Characters, One Number and One Special Case Character "
         )
         .strict(true),
 });
