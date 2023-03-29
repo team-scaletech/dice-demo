7f176277945521a070e7947e7f23913afb8581f0
rita-scaletech <rita.savaliya@scaletech.xyz>
2023-03-29 14:45:03 +0530

    add number and email valid validation

--- src/shared/constants/validation-schema.ts
+++ src/shared/constants/validation-schema.ts
@@ -7,18 +7,16 @@ const loginFormValidationSchema = Yup.object().shape({
 
 const signUpFormValidationSchema = Yup.object().shape({
     username: Yup.string().required('Please Enter Your Name').strict(true),
-    email: Yup.string().required('Please Enter email').strict(true),
-    mobileNo: Yup.string().required('Please Enter Mobile Number').strict(true),
-    // mobileNo: Yup.number()
-    //     .typeError("That doesn't look like a phone number")
-    //     .positive("A phone number can't start with a minus")
-    //     .integer("A phone number can't include a decimal point")
-    //     .min(10)
-    //     .required('Please Enter Your Mobile Number'),
-    // email: Yup.string()
-    //     .required('Please Enter Email')
-    //     .strict(true)
-    //     .email('Please Enter Valid Email'),
+    mobileNo: Yup.number()
+        .typeError("That doesn't look like a phone number")
+        .positive("A phone number can't start with a minus")
+        .integer("A phone number can't include a decimal point")
+        .min(10)
+        .required('Please Enter Your Mobile Number'),
+    email: Yup.string()
+        .required('Please Enter Email')
+        .strict(true)
+        .email('Please Enter Valid Email'),
     password: Yup.string().required('Please Enter Password').strict(true),
 });
 
